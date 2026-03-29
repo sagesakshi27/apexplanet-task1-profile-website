b281c8785ab1a58d9706cfa254aed0107786cf65
Chavan Sakshi Pandurang <sakshichavan4151@gmail.com>
2026-03-29 14:07:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function showMessage() {
+    const message = document.getElementById("message");
+    message.innerText = "Thanks for reaching out! I’ll get back to you soon 😊";
+}
 
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+    const btn = document.getElementById("themeBtn");
+
+    if (document.body.classList.contains("dark")) {
+        btn.innerText = "Light Mode";
+    } else {
+        btn.innerText = "Dark Mode";
+    }
+}
+
+document.getElementById("themeBtn").addEventListener("click", toggleTheme);
+document.getElementById("contactBtn").addEventListener("click", showMessage);
